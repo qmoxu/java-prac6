8035c1129b8c8efb313544a597649257ba717d56
qmocu <vvprokop2006@gmail.com>
2025-06-26 01:21:19 +0300

    plugin success 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+/**
+ * Utility class to run the application.
+ */
+public final class Main
+{
+    /**
+     * The main method that prints a message to the console.
+     *
+     * @param args Command-line arguments (not used).
+     */
+    public static void main(final String[] args)
+    {
         System.out.println("bla bla bla");
     }
 }

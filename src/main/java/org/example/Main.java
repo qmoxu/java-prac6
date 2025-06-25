984a518222cdf6ae444943e79d444f43b2e28fa1
qmocu <vvprokop2006@gmail.com>
2025-06-26 01:29:01 +0300

    plugin success 6???

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,19 @@ package org.example;
 /**
  * Utility class to run blablabla.
  */
-public final class Main {
+public final class Main
+{
     /**
      * The main method that prints a message to the console.
      * @param args Cli arguments.
      */
-    public static void main(final String[] args) {
+    public static void main(final String[] args)
+    {
         System.out.println("bla bla bla");
     }
 
-    private Main() {
+    private Main()
+    {
         throw new AssertionError("Utility class should not be instantiated.");
     }
 }
